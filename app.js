315beb45c67d475829b360d695db0d8c28dccad6
Max Edmonds <maxedmonds05@gmail.com>
2020-07-11 19:43:15 -0700

    arrange prompt questions

--- app.js
+++ app.js
@@ -11,35 +11,16 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 const Employee = require("./lib/Employee");
 var employees = []
+// render(employees);
 // Initialize a new employee role
 
-
+console.log('Please enter informaton for your team members')
 inquirer.prompt([
     {
         type: 'input',
         name: 'name',
-        message: 'What is your name?'
+        message: 'Good day Manager, what is your name?'
     },
-    {
-        type: 'list',
-        name: 'role',
-        choices: ['Intern', 'Engineer', 'Manager']
-    },
-
-    // {
-    //     type: 'input',
-    //     name: 'github',
-    //     message: 'Hey Engineer, what is your Github username?',
-    //     when: (answers) => prompt.choices === 'Engineer'
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'officeNumber',
-    //     message: 'Hey Manager, what is your Office number?',
-    //     when: function (response) {
-    //         if (response.role === Manager);
-    //     }
-    // },
     {
         type: 'input',
         name: 'email',
@@ -47,31 +28,45 @@ inquirer.prompt([
     }, {
         type: 'input',
         name: 'id',
-        message: 'What your employee id number?'
+        message: 'What your is your employee id number?'
+    },
+
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'What is your office number?'
+    },
+    {
+        type: 'list',
+        name: 'role',
+        choices: ['Intern', 'Engineer', 'No more team members to add']
     }
 
 ]).then(function (response) {
-    console.log('response is:', response);
-
     switch (response.role) {
         case "Engineer":
-            inquirer.prompt({ type: 'input', name: 'github', message: 'hey engineer, what is your github username?' }).then(function (engineer) {
-                var developer = new Engineer(response.name, response.id, response.email, engineer.github);
-                employees.push(developer);
+            inquirer.prompt({ type: 'input', name: 'github', message: 'What is the github username of the engineer?' }).then(function (engineer) {
+                let employee = new Engineer(response.name, response.id, response.email, engineer.github);
+                employees.push(employee);
             })
             break;
         case "Intern":
-            inquirer.prompt({ type: 'input', name: 'school', message: 'Intern, which school did your parents send you to?' }).then(function (intern) {
-                var coffeePusher = new Intern(response.name, response.id, response.email, intern.school);
-                employees.push(coffeePusher);
+            inquirer.prompt({ type: 'input', name: 'school', message: 'Which school did the intern attend?' }).then(function (intern) {
+                let employee = new Intern(response.name, response.id, response.email, intern.school);
+                employees.push(employee);
             })
             break;
-        case "Manager":
+        case "No more team members to add":
+
+            render(employees);
             break;
         default:
             break;
-    }
-});
+    };
+})
+
+
+
 
 //push employees to an array, push that array to the render function. then you're done. MAKE SURE THE ORDER OF YOUR NEW CONSTRUCTORS MATCHES THE ORIGINAL CLASS
 
